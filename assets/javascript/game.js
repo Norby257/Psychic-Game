dbcdecb5a852693a75d811092ac438f4d5050702
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-02 16:40:52 -0600

    more changes to variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,24 @@
-/* variables I need:
-playerChoice // is onkeyup
-computerChoice  //output of random function on Choices array
-Choices array of letters from A to Z
-Wins number 
-Loses number 
-Guesses Left  == guesses - numGuess 
-currentGuess string 
-Guesss = 9 
-numGuess 
-
-win 
-	if playerchoice === computerchoice
-		add one to win variable -> win++
-
-		restart game without refreshing the page 
-		
-lose
-		if (var i = 0; i < 9; i++) 
-		playerChoice !== computerChoice for all guesses
-		add one to lose variable lost++
-		restart game  without refershing the page
-
-currentguess 
-	console.log(playerchoice, ,); 
-		if wrong, keep going until all options exhaustd
-		if right, clear input 
 
 
 
 
-*/ 
-
-
-// setting up variables and data structures 
-//moving some var to global vars 
-//fixed indentation
 	
 	var wins = 0;
 	var losses = 0;
 	var guessesLeft = 9;
-	// var playerChoice = event.key;
+	var playerChoice;
 	var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
 	
 	//get user choice and computer choice
 	document.onkeyup = function(event) {
-		console.log(event);
 		var playerChoice = event.key;
-
-		// guessesLeft--;
-		
+		var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 		if (playerChoice === computerchoice) { 
 			wins++; //if correct add one to wins
 			guessesLeft = 9; //reset var 
-			var computerChoice = choices[Math.floor(Math.random() * choices.length)]; //comp picks diff rando num
+			 //comp picks diff rando num
 		}
 		else if (playerChoice != computerchoice) {
 			
@@ -74,13 +38,6 @@ currentguess
 		<p> losses ${losses} </p>
 		`;
 
-
-
-	// if (playerChoice === computerChoice) {
-	// 	win++; //add one to wins
-	// 	guessesLeft = 9; //re-set guesses left 
-	// 	computerchoice = choices[Math.floo(Math.random() * choices.length)]; // computer picks a new rando num
-	// }	var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
 
 	
