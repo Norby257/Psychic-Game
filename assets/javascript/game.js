c4c8933d37b559cb7be0256b55fc90843452fca4
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-02 17:30:51 -0600

    added comments and cleaned code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,41 @@
-
-
-
-
-	
+	var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	var wins = 0;
 	var losses = 0;
 	var guessesLeft = 9;
-	var playerChoice;
-	var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+	var computerGuess;
+	var playerGuess;
 
-	
-	//get user choice and computer choice
+
+	// this function is run when a user presses a key 
 	document.onkeyup = function(event) {
-		var playerChoice = event.key;
-		var computerChoice = choices[Math.floor(Math.random() * choices.length)];
-		if (playerChoice === computerchoice) { 
-			wins++; //if correct add one to wins
-			guessesLeft = 9; //reset var 
-			 //comp picks diff rando num
-		}
-		else if (playerChoice != computerchoice) {
-			
-				guessesLeft--;
-		}
-		losses++ //if all choices are wrong, add one to losses 
-		guessesLeft = 9;
-		var computerChoice = choices[Math.floor(Math.random() * choices.length)];
-	}
-
-
-		//display user choices
-	
-	document.getElementById("game").innerHTML = `
-		<p> Your Guesses So Far: ${playerChoice} </p>
-		<p> Wins: ${wins} </p>
-		<p> losses ${losses} </p>
-		`;
+			//computer randomly picks a letter from the array of choices
+			var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	        //determine which key was pressed stored in playerchoice variable (within scope of this function)
+	        var playerGuess = event.key;
+	        // check if player is correct. if player is correct, add one to wins, re-set guessesLeft and make computer pick another letter.
+	        if (playerGuess === computerGuess) {
+	        	wins++;
+	        	guessesLeft = 9;
+	        	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 
 
-	
+	        }
+	        else {
+	        	guessesLeft--;
+	        	if (guessesLeft = 0) {
+	        		losses++;
+	        		guessesLeft = 9;
+	        		var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 
+	        	}
+	        }
+	 };
 
-	
-
+	 //display user guess, wins, losses, 
+	 document.getElementById("game").innerHTML = `
+	 <p> Wins: ${wins} </p>
+	 <p> Losses: ${losses} </p>
+	 <p> GuessesLeft: ${guessesLeft} </p>
+	 `;
