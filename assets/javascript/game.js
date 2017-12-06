03bbc87a92afdd17b3c720bc580e6261e57c7f0c
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-05 21:50:08 -0600

    fixed onkeyup function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-	var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log("we are linked");
+var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	var wins = 0;
 	var losses = 0;
 	var guessesLeft = 9;
@@ -7,11 +8,27 @@
 
 
 	// this function runs when a user presses a key 
+	//ok let's console.log the user pressing the key - if nothing is appearing then it means the code is not done.
 	document.onkeyup = function(event) {
+		var playerGuess = event.key;
+		console.log(playerGuess);
+		var computerGuess = choices[Math.floor(Math.random() * computerChoices.length)];
+		console.log(computerGuess);
+
+
+
+		
+	}
+
+	//and then let's try displaying with the html 
+
+
+
+
+///*	document.onkeyup = function(event) {
 			//computer randomly picks a letter from the array of choices
-			var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 	        //determine which key was the player chose, store it in playerguess variable (within scope of this function)
-	        var playerGuess = event.key;
+	      /*  var playerGuess = event.key;
 	        // check if player is correct. if player is correct, add one to wins, re-set guessesLeft and make computer pick another letter.
 	        if (playerGuess === computerGuess) {
 	        	wins++;
@@ -23,19 +40,15 @@
 	        }
 	        else {
 	        	guessesLeft--;
-	        	if (guessesLeft = 0) {
-	        		losses++;
-	        		guessesLeft = 9;
-	        		var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
-	        	}
+	        	
 	        }
 	 };
 
-	 //display user guess, wins, losses, 
+	// display user guess, wins, losses, 
 	 document.getElementById("game").innerHTML = `
 	 <p> Wins: ${wins} </p>
 	 <p> Losses: ${losses} </p>
 	 <p> GuessesLeft: ${guessesLeft} </p>
 	 `;
+
+	
