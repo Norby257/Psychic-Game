67cc10fc346d5c4d2130396d8d5f23d5e0edd285
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-05 22:07:23 -0600

    added alerts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,48 +7,10 @@ var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 	var playerGuess;
 
 
-	// this function runs when a user presses a key 
-	//ok let's console.log the user pressing the key - if nothing is appearing then it means the code is not done.
+
 	document.onkeyup = function(event) {
-		var playerGuess = event.key;
-		console.log(playerGuess);
-		var computerGuess = choices[Math.floor(Math.random() * computerChoices.length)];
-		console.log(computerGuess);
-
-
-
+		alert("you pressed a key");
+		console.log(event.key);
 		
-	}
-
-	//and then let's try displaying with the html 
-
-
-
-
-///*	document.onkeyup = function(event) {
-			//computer randomly picks a letter from the array of choices
-	        //determine which key was the player chose, store it in playerguess variable (within scope of this function)
-	      /*  var playerGuess = event.key;
-	        // check if player is correct. if player is correct, add one to wins, re-set guessesLeft and make computer pick another letter.
-	        if (playerGuess === computerGuess) {
-	        	wins++;
-	        	guessesLeft = 9;
-	        	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
-
-	        }
-	        else {
-	        	guessesLeft--;
-	        	
-	        }
-	 };
-
-	// display user guess, wins, losses, 
-	 document.getElementById("game").innerHTML = `
-	 <p> Wins: ${wins} </p>
-	 <p> Losses: ${losses} </p>
-	 <p> GuessesLeft: ${guessesLeft} </p>
-	 `;
-
 	
+	}
