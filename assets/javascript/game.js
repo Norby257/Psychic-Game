ebc1d9f93feff6c2b21d74d5021f4961f69a267a
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-06 16:41:05 -0600

    added conditionals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,41 @@
 console.log("we are linked");
 var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	var wins = 0;
-	var losses = 0;
-	var guessesLeft = 9;
-	var computerGuess;
-	var playerGuess;
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var computerGuess;
+var playerGuess;
 
 
 
 	document.onkeyup = function(event) {
-		alert("you pressed a key");
-		console.log(event.key);
+		 console.log(event);
+		 var playerGuess = event.key;
+		 var computerGuess = choices[Math.floor(Math.random() * choices.length)];
+		 if (playerGuess === computerGuess) {
+				wins++;
+				guessesLeft = 9;
+				var computerGuess = choices[Math.floor(Math.random() * choices.length)];
 		
+		 } else if(guessesLeft = 0)  {
+
+				losses++;
+				var computerGuess = choices[Math.floor(Math.random() * choices.length)];
+				
+
+			}
+		
+		  else {
+			 guessesLeft--;
+		 }
 	
 	}
+
+	document.getElementById("game").innerHTML = `
+	<p> Your Guesses So Far: ${playerGuess}</p>
+	<p> Wins: ${wins}</p>
+	<p> Losses: ${losses} </p>
+	<p> Guesses Left: ${guessesLeft} </p>	
+	`;
+
+	
